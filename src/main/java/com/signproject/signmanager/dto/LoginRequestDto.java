5832c5385ff0b759e884603245c89056b346d4e1
DESKTOP-1343178\96tmd <96tmdqh@naver.com>
2025-04-16 18:29:36 +0900

    로그인 요청 정보를 담는 LoginRequestDto 클래스 추가

--- src/main/java/com/signproject/signmanager/dto/LoginRequestDto.java
+++ src/main/java/com/signproject/signmanager/dto/LoginRequestDto.java
@@ -1,4 +1,17 @@
 package com.signproject.signmanager.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 로그인 요청 시 클라이언트로부터 전달받는 데이터 구조
+ */
+@Getter
+@NoArgsConstructor
 public class LoginRequestDto {
+
+    private String username;
+    private String password;
+
+    // validation, constructor, builder 등은 필요 시 확장 가능
 }
