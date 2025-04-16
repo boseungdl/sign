e617761aa4e2af8265d0f1ad6a4cbb9e91865741
DESKTOP-1343178\96tmd <96tmdqh@naver.com>
2025-04-16 16:58:26 +0900

    Initial commit : 프로젝트 구조 및 기본(DB) 설정

---
+++ src/test/java/com/signproject/signmanager/SignmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signproject.signmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
